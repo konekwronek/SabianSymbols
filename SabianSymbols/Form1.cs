9585a476070f9ede141f2870383dd765dc75148c
Konrad Wroński <konwron@gmail.com>
2022-05-09 15:51:30 +0200

    Dodaj pliki projektów.

---
+++ SabianSymbols/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SabianSymbols
+{
+    public partial class SabianSymbols : Form
+    {
+        public SabianSymbols()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            number++;
+            if (number == 361) number = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = number.ToString();
+        }
+    }
+}
